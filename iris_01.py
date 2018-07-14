5a75809513558d12555239c91e1b584f5e984b67
Sam <sam6238@ms28.hinet.net>
2018-07-14 14:26:37 +0800

    Create iris_01.py

---
+++ iris_01.py
@@ -0,0 +1,7 @@
+#matplotlib
+import matplotlib.pyplot as pyplot
+from mpl_toolkits.mplot3d import Axes3D
+#pip install C:\Users\StudyRoom\Downloads\Twisted-18.7.0-cp37-cp37m-win32.whl
+#pip install SciPy
+#sklearn
+from sklearn import datasets
